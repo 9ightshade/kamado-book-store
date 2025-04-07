c7810442ce4a1deb02db95705f73f3577cbb2492
9ightshade <aniekemeusanga@gmail.com>
2025-04-07 12:00:22 +0100

    sizing for image

--- components/BookDetail.tsx
+++ components/BookDetail.tsx
@@ -86,6 +86,8 @@ export default function BookDetail({ bookId }: BookDetailProps) {
         <div className="md:w-1/3">
           {book.coverImageId ? (
             <Image
+              width={300}
+              height={300}
               src={
                 bookService.getImagePreview(book.coverImageId) ||
                 "/placeholder-image.png"
